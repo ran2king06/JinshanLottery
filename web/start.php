75276f216cf46d8b37c581eca6d0634f409770fc
ran2king06 <ran2king06@gmail.com>
2023-08-04 21:24:20 +0800

    Start frontend project

---
+++ web/start.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>來趣金山老街</title>
+</head>
+<body>
+  
+</body>
+</html>
